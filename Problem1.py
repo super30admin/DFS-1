3bb4362204515484a3c29e65d54d001ea850c872
Mudita Bysani <muditabysani@Muditas-MacBook-Pro.local>
2019-08-31 10:28:59 -0700

    Add the DFS approach for solving 01-matric

--- Problem1.py
+++ Problem1.py
@@ -2,33 +2,38 @@ from collections import deque
 
 class Solution:
 	def floodFill1(self, image, sr, sc, newColor):
+		# Time Complexity : O(mn) where m is the number of rows and n is the number of columns
+		# Space Complexity : O(mn) where m is the number of rows and n is the number of columns
+		# Accepted on leetcode
+		# BFS Approach
 		queue = deque()
 		queue.append([sr, sc])
 		rows = len(image)
 		columns = len(image[0])
 		val = image[sr][sc]
-		if val == newColor:
+		if image == None or image[0] == None or val == newColor:
 			return image
-		image1 = [x[:] for x in image]
 		while len(queue) > 0:
-			size = len(queue)
-			for i in range(size):
-				r, c = queue.popleft()
-				if r > 0 and image[r - 1][c] == val:
-					queue.append([r - 1, c])
-				if r < len(image) - 1 and image[r + 1][c] == val:
-					queue.append([r + 1, c])
-				if c > 0 and image[r][c - 1] == val:
-					queue.append([r, c - 1])
-				if c < len(image[0]) - 1 and image[r][c + 1] == val:
-					queue.append([r, c + 1])
-				image1[r][c] = newColor
-			print(len(queue))
-		return image1
+			r, c = queue.popleft()
+			image[r][c] = newColor
+			if r > 0 and image[r - 1][c] == val:
+				queue.append([r - 1, c])
+			if r < len(image) - 1 and image[r + 1][c] == val:
+				queue.append([r + 1, c])
+			if c > 0 and image[r][c - 1] == val:
+				queue.append([r, c - 1])
+			if c < len(image[0]) - 1 and image[r][c + 1] == val:
+				queue.append([r, c + 1])
+		return image
 
 	def floodFill2(self, image, sr, sc, newColor):
+		# Time Complexity : O(mn) where m is the number of rows and n is the number of columns
+		# Space Complexity : O(mn) where m is the number of rows and n is the number of columns
+		# Accepted on leetcode
+		# DFS Approach
 		color = image[sr][sc]
 		if color == newColor:
+			# If the new color is same as the existing color no changes is needed
 			return image
 		self.helper(image, sr, sc, newColor, color)
 		return image
