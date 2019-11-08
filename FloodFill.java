10742fd048ca0a14cfd5d4da82a936cac71e4158
Ajay Chauhan <chauhanajayvikram1992@gmail.com>
2019-11-07 16:46:26 -0800

    Added comments

--- FloodFill.java
+++ FloodFill.java
@@ -1,20 +1,25 @@
+//Time Complexity : O(m*n)
+//Space Complexity : O(1)( ignoring the recursive stack)
+//Did this code successfully run on Leetcode :yes
+//Any problem you faced while coding this : 
+
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         
              if (image[sr][sc] != newColor)
-            dfs(image, sr, sc, image[sr][sc], newColor);
+            helper(image, sr, sc, image[sr][sc], newColor);
         return image;
     }
 
-    private void dfs(int[][] image, int i, int j, int c0, int c1) {
+    private void helper(int[][] image, int i, int j, int c0, int c1) {
         
         int m = image.length;
         int n = image[0].length;
         if (i < 0 || j < 0 || i >= m || j >= n || image[i][j] != c0) return;
         image[i][j] = c1;
-        dfs(image, i, j - 1, c0, c1);
-        dfs(image, i, j + 1, c0, c1);
-        dfs(image, i - 1, j, c0, c1);
-        dfs(image, i + 1, j, c0, c1);
+        helper(image, i, j - 1, c0, c1);
+        helper(image, i, j + 1, c0, c1);
+        helper(image, i - 1, j, c0, c1);
+        helper(image, i + 1, j, c0, c1);
     }
 }
