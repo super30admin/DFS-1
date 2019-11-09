33894c7c451775adc820f6ef92ff35e8f7e8d77a
Akhilesh Borgaonkar <aborgaonkar@onemarketnetwork.com>
2019-11-09 14:53:54 -0800

    fixed 01 matrix failure

--- 01Matrix2.java
+++ 01Matrix2.java
@@ -4,7 +4,7 @@ Problem: LC 542. 01 Matrix (DFS-1)
 Approach: Using iterative BFS approach here to change the existing distances-to-zero in the matrix with the current minimum.
 Time Complexity: O(m*n) where m and n are vertical and horizontal length of matrix.
 Space complexity: O(1) constant.
-Known Issue: It works for small matrix but gives TLE for bigger matrices. Will work on it.
+LC verified.
 */
 
 class Solution {
@@ -40,13 +40,10 @@ class Solution {
                     //calculate the coordinates of neighbors
                     int x = curr[0] + dir[0];
                     int y = curr[1] + dir[1];
-                    if(x >= 0 && y>=0 && x<m && y<n){	//avoid processing of invalid coordinates
-                        
-                        if(matrix[x][y] != 0 && matrix[x][y] != 1){	//ignoring the neighbors with original 1 value
-                            
-                            matrix[x][y] = Math.min(matrix[curr[0]][curr[1]]+1, matrix[x][y]);	//assigning the new min distance to zero
-                            q.add(new int[]{x,y});		//add the ccordinates in queue to perform BFS on its neighbors
-                        }
+                    if(x >= 0 && y>=0 && x<m && y<n && matrix[x][y] >= matrix[curr[0]][curr[1]]+1){	//avoid processing of invalid coordinates
+                                                    
+                        matrix[x][y] = Math.min(matrix[curr[0]][curr[1]]+1, matrix[x][y]);	//assigning the new min distance to zero
+                        q.add(new int[]{x,y});		//add the ccordinates in queue to perform BFS on its neighbors
                     } 
                     
                 }
