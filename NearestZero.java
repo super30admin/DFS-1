33efaf9bdc4285ceb4ff6a61627382036325dbcf
keerthitejakonuri <keerthi49100@gmail.com>
2019-11-09 21:08:19 -0600

    Update NearestZero.java
    Nearest zero with O(M*N) Time complexity solution

--- NearestZero.java
+++ NearestZero.java
@@ -1,5 +1,5 @@
 // Time Complexity :O(row*col).
-// Space Complexity :O(row*row*col*col).
+// Space Complexity :O(row*col).
 // Did this code successfully run on Leetcode :Yes.
 // Any problem you faced while coding this :Nope.
 
@@ -7,7 +7,41 @@
 // Your code here along with comments explaining your approach
 
 class NearestZero {
-    public int[][] updateMatrix(int[][] matrix) {
+	
+	 public int[][] updateMatrix(int[][] matrix) {
+	        if(null == matrix || 0 == matrix.length){return matrix;}
+	        Queue<int[]> q = new LinkedList<>();
+	        for(int i = 0; i < matrix.length; i++){
+	            for(int j = 0; j < matrix[0].length; j++){
+	                if(matrix[i][j] != 0){
+	                    matrix[i][j] = Integer.MAX_VALUE;
+	                }else{
+	                    q.offer(new int[]{i,j});
+	                }
+	            }
+	        }
+	        int dirs[][] = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+	        while(!q.isEmpty()){
+	            int size = q.size();
+	            for(int k = 0; k < size; k++){
+	            int point[] = q.poll();
+	            int newDistance = matrix[point[0]][point[1]] + 1;
+	            for(int dir[]: dirs){
+	                int r = point[0] + dir[0];
+	                int c = point[1] + dir[1];
+	                if(r >= 0 && r < matrix.length && c >= 0 && c < matrix[0].length){
+	                 if(matrix[r][c] != 0 && newDistance < matrix[r][c]){
+	                    matrix[r][c] = newDistance;
+	                     q.offer(new int[]{r,c});
+	                 }   
+	                }
+	            }
+	         }
+	        }
+	        return matrix;
+	    }
+	
+    public int[][] updateMatrix1(int[][] matrix) {
         if(null == matrix || 0 == matrix.length){return matrix;}
         int dist[][] = new int[matrix.length][matrix[0].length];
         for(int i = 0; i < matrix.length; i++){
