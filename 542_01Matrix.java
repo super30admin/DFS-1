d4c60a1de81570dfb79e030a8bbf22a96f0ecefe
yosnehal <sgandham@uci.edu>
2020-05-21 02:28:35 -0700

    modifying the code

--- 542_01Matrix.java
+++ 542_01Matrix.java
@@ -7,54 +7,48 @@
 // Your code here along with comments explaining your approach
 
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+    public int[][] updateMatrix(int[][] matrix) {
         
-        // dfs(image, image.length, image[0].length, sr, sc, image[sr][sc], newColor, new HashSet<Integer>());
+        int row = matrix.length;
+        int col = matrix[0].length;
         
-        Queue<Integer> q = new LinkedList<>();
-        int rowLen = image.length;
-        int colLen = image[0].length, connected=image[sr][sc];
-        if(connected==newColor)
-            return image;
-        int ax[] = {-1,0,1,0};
-        int ay[] = {0,1,0,-1};
+        int[][] res = new int[row][col];
+        int[][] dir = {{0,1},{1,0},{0,-1},{-1,0}};
         
-        q.add(sr*colLen+sc);
-        while(!q.isEmpty()){
-            int val = q.poll();
-            int x1 = val/colLen;
-            int y1 = val%colLen;
-            image[x1][y1]=newColor;
-            
-            for(int i=0;i<4;i++){
-             int x=x1+ax[i];
-             int y=y1+ay[i];
-            
-                if(x>=0 && y>=0 && x<rowLen && y<colLen && image[x][y]==connected){
-                    q.add(x*colLen+y);
+        Queue<Pair<Integer,Integer>> q = new LinkedList<>();
+        
+        for(int i=0;i<row;i++) {
+            for(int j=0;j<col;j++) {
+                
+                if(matrix[i][j] == 0) {
+                    
+                    q.add(new Pair(i,j));
                 }
             }
-            
         }
-        return image;
-        
-    }
-    
-    
-    public void dfs(int[][] image, int rowLen, int colLen, int sr, int sc, int connected, int newColor, HashSet<Integer> set) {
-        int ax[] = {-1,0,1,0};
-        int ay[] = {0,1,0,-1,0};
-        image[sr][sc] = newColor;
-        if(!set.add(sr*colLen+sc))
-            return;
-        for(int i=0;i<4;i++){
-            int x=sr+ax[i];
-            int y=sc+ay[i];
+
+        while(!q.isEmpty()) {
             
-            if(x>=0 && y>=0 && x<rowLen && y<colLen && image[x][y]==connected){
-                image[x][y]=newColor;
-                dfs(image,rowLen,colLen,x,y,connected, newColor, set);
+            Pair<Integer,Integer> p = q.poll();
+            int r = p.getKey();
+            int c = p.getValue();
+
+            for(int j=0;j<4;j++) {
+
+                int ro = r+dir[j][0];
+                int co = c+dir[j][1];
+
+                if(ro<0 || co <0 || ro >= row || co >= col || matrix[ro][co] == 0)
+                    continue;
+
+                res[ro][co] = res[r][c]+1;
+                matrix[ro][co] = 0;
+                q.add(new Pair(ro,co));
+
             }
         }
+        
+        return res;
     }
+    
 }
