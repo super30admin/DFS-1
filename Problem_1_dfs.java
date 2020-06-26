1a7db307a5d10e41e370ef59ee013da356035cfe
Sarthak Sugandhi <47070701+sarthaksugandhi9@users.noreply.github.com>
2020-06-25 17:06:39 -0700

    Create Problem_1_dfs.java

---
+++ Problem_1_dfs.java
@@ -0,0 +1,30 @@
+// Time complexity - O(m*n)
+// Space complexity - O(m*n)
+
+// DFS
+
+class Solution {
+    int color;
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if(image == null || image[sr][sc] == newColor) return image;
+        color = image[sr][sc];
+        
+        dfs(image,sr,sc,newColor);
+        return image;
+    }
+    
+    private void dfs(int[][] image, int sr, int sc, int newColor){
+        // base
+        if(sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != color ){
+            return;
+        }
+        // logic
+        int[][] dirs = {{0,1},{0,-1},{-1,0},{1,0}};
+        image[sr][sc] = newColor;
+        for(int[] dir : dirs){
+            int r = dir[0] + sr;
+            int c = dir[1] + sc;
+            dfs(image,r,c,newColor);
+        }
+    }
+}
