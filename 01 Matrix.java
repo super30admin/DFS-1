e7a249ef798962fd9f0fd9ae5fd343795b29adbb
Achyuth72 <68582633+Achyuth72@users.noreply.github.com>
2020-11-13 10:13:38 -0700

    Create 01 Matrix.java

---
+++ 01 Matrix.java
@@ -0,0 +1,51 @@
+// Time Complexity :O(m*n)
+// Space Complexity :O(m*n)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :No
+
+
+class Solution {
+    public int[][] updateMatrix(int[][] matrix) {
+        if(matrix==null || matrix.length==0) return matrix;
+        int m=matrix.length; int n=matrix[0].length;
+        Queue<int[]> q=new LinkedList<>();
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    q.add(new int[]{i,j});
+                }
+                else
+                {
+                    matrix[i][j]=-1;
+                }
+            }
+        }
+    int[][] dirs={{0,1},{0,-1},{1,0},{-1,0}};
+        while(!q.isEmpty())
+        {
+            int dist=1;
+            int size=q.size();
+            for(int i=0;i<size;i++)
+            {
+                int[] curr=q.poll();
+                
+                for(int[] dir:dirs)
+                {
+                    int r=dir[0]+curr[0];
+                    int c=dir[1]+curr[1];
+                    
+                    if(r>=0 && c>=0 && r<m && c<n && matrix[r][c]==-1)
+                    {
+                        q.add(new int[]{r,c});
+                        matrix[r][c]=dist;
+                    }
+                }
+            }
+            dist++;
+        }
+       return matrix; 
+    }
+}
