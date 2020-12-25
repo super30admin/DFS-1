2bb30696e43c635a2b1033faddaa9e0996b27c66
Vineet Shah <shahvineet95@gmail.com>
2020-12-25 13:13:24 -0500

    completed DFS-1 problems

---
+++ updateMatrix.java
@@ -0,0 +1,46 @@
+// Time Complexity : O(MN)
+// Space Complexity : O(MN)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :no
+
+
+// Your code here along with comments explaining your approach
+class Solution {
+    public int[][] updateMatrix(int[][] matrix) {
+        if(matrix == null || matrix.length == 0){
+            return matrix;
+        }
+        Queue<int[]> queue = new LinkedList<>();
+        int m = matrix.length, n = matrix[0].length;
+        for(int i = 0;i < m; i++){
+            for(int j = 0;j < n; j++){
+                
+                if(matrix[i][j] == 0){
+                    queue.add(new int[] {i,j});
+                }
+                else{
+                    //If we encounter 1 make it -1
+                    matrix[i][j] = -1;
+                }
+            }
+        }
+        int dist = 1;
+        int[][] dirs = {{0,1}, {1,0}, {-1,0},{0,-1}};
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                int curr[] = queue.poll();
+                for(int[] dir: dirs){
+                    int r = curr[0] + dir[0];
+                    int c = curr[1] + dir[1];
+                    if(r >= 0 && r < m && c >= 0 && c < n && matrix[r][c] == -1){
+                        matrix[r][c] = dist;
+                        queue.add(new int[] {r,c});
+                    }
+                }
+            }
+            dist++;
+        }
+        return matrix;
+    }
+}
