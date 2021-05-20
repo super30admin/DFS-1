88a50823357d18a4c29792d9b0e44555266f7ab5
shubhamgupta18 <shubhamgupta202017@gmail.com>
2021-05-20 14:55:22 -0500

    DFS 1 completed. Please review.

---
+++ zeroOneMatrixBFS.java
@@ -0,0 +1,65 @@
+// Time complexity - O(mxn)
+// Space complexity - O(mxn) 
+//Did this code successfully run on Leetcode : Yes
+//Any problem you faced while coding this : No
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        
+       
+        
+        if(image.length == 0|| image[0].length == 0 || image == null){
+            
+            return image;
+        }
+        
+        //Queue<Integer> q1 = new LinkedList<>();
+        int r = image.length;
+        int c = image[0].length;
+        
+        int oldColor = image[sr][sc];
+        image[sr][sc] = newColor;
+        if(oldColor == newColor){
+            
+            return image;
+        }
+      //  q1.add(sr);
+       // q1.add(sc);
+        dfs(image, sr, sc, newColor, oldColor, r, c);
+        
+        return image;
+    }
+    private void dfs(int[][] image, int sr, int sc, int newColor, int oldColor, int r, int c){
+        
+        
+        int[] dx = new int[]{-1,0,0,1};
+        int[] dy = new int[]{0,1,-1,0};
+        
+     
+            for(int i = 0; i < 4; i ++){
+                
+                int ndx = sr + dx[i];
+                int ndy = sc + dy[i];
+                
+                if(isValid(r,c,ndx,ndy)){
+                    
+                    if(image[ndx][ndy] == oldColor){
+                        image[ndx][ndy] = newColor;
+                          dfs(image, ndx, ndy, newColor, oldColor, r, c);
+                    
+                    
+                    
+                }
+                
+            }
+        }
+        
+        
+    }
+    private boolean isValid(int r, int c, int ndx, int ndy){
+        if(ndx < r && ndx > -1 && ndy < c && ndy > -1){
+            
+            return true;
+        }
+        return false;
+    }
+}
