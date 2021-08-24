da1d68d0e7098c09db82543a5560f3f7aad84a04
Durgesh Patil <patildurgesh1998@gmail.com>
2021-08-24 09:24:07 -0400

    Update Problem-1.cpp

--- Problem-1.cpp
+++ Problem-1.cpp
@@ -30,3 +30,45 @@ public:
         return image;
     }
 };
+
+/*
+Time Complexity = O(M*N)
+Space Complexity =O(M*N)
+Where M is the number of rows and N is the number of columns in the matrix.
+*/
+class Solution {
+public:
+    int m,n;
+    int color;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        if(image[sr][sc]==newColor)
+            return image;
+        color = image[sr][sc];
+        m = image.size();
+        n = image[0].size();
+        int size,i,j;
+        queue<vector<int>> q;
+        vector <vector<int>> dirs{{0,1}, {1,0}, {-1,0}, {0,-1}};
+        q.push({sr,sc});
+        image[sr][sc] = newColor;
+        while(!q.empty())
+        {
+            size = q.size();
+            vector<int> temp(q.front());
+            q.pop();
+            for(i=0;i<size;i++)
+            {
+                for(j=0;j<4;j++){
+                int r = temp[0] +  dirs[j][0];
+                int c = temp[1] + dirs[j][1];
+                    if(r>=0 && r<m && c>=0 && c<n && image[r][c]==color)
+                    {
+                        q.push({r,c});
+                        image[r][c] = newColor;
+                    }
+                }
+            }
+        }
+        return image;
+    }
+};
