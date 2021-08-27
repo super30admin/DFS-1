83f0a23cafce7890c1a3f2cc396faac14123404f
Priyanka <pzar1@asu.edu>
2021-08-27 15:39:07 -0700

    DFS-1 completed

---
+++ 01matrix.py
@@ -0,0 +1,37 @@
+from queue import Queue
+import sys
+
+class Solution:
+    """
+    Time complexity: no. of zeroes * O(r*c)
+    Space complexity: O(1)
+    """
+    def updateMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
+        if matrix is None or len(matrix) is 0:
+            return matrix
+        
+        r = len(matrix)
+        c = len(matrix[0])
+        q = Queue()
+        
+        for i in range(r):
+            for j in range(c):
+                if matrix[i][j] is 0:
+                    q.put((i,j))
+                elif matrix[i][j] is 1:
+                    matrix[i][j] = sys.maxsize
+            
+        dir = [[0,1], [1,0], [0,-1], [-1,0]]
+        while not q.empty():
+            
+            front = q.get()
+            
+            for dirs in dir:
+                i = front[0] + dirs[0]
+                j = front[1] + dirs[1]
+            
+                if i >= 0 and i < r and j >= 0 and j < c and matrix[front[0]][front[1]] + 1 < matrix[i][j]:
+                    q.put((i,j))
+                    matrix[i][j] = matrix[front[0]][front[1]] + 1
+    
+        return matrix
---
+++ floodfill.py
@@ -0,0 +1,27 @@
+class Solution:
+    """
+    Time complexity: O(r*c)
+    Space complexity: O(r*c)
+    """
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        if image == None or len(image) == 0:
+            return image
+        
+        if image[sr][sc] == newColor:
+            return image
+        
+        oldColor = image[sr][sc]
+        
+        def recurr(image, i, j):
+            directions = [[0,1], [1,0], [-1,0], [0,-1]]
+            image[i][j] = newColor
+
+            for dir in directions:
+                r = i + dir[0]
+                c = j + dir[1]
+
+                if r >= 0 and r < len(image) and c >= 0 and c < len(image[0]) and image[r][c] == oldColor:
+                    recurr(image, r, c)
+        
+        recurr(image, sr, sc)
+        return image
