88d3b073b527d1fb39b047b9a0ed00e92f81bf8c
Priyanka <pzar1@asu.edu>
2021-08-28 10:44:33 -0700

    Completed DFS - 1

--- floodfill.py
+++ floodfill.py
@@ -1,19 +1,20 @@
 class Solution:
     """
     Time complexity: O(r*c)
-    Space complexity: O(r*c)
+    Space complexity: O(1)
     """
+
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
         if image == None or len(image) == 0:
             return image
-        
+
         if image[sr][sc] == newColor:
             return image
-        
+
         oldColor = image[sr][sc]
-        
+
         def recurr(image, i, j):
-            directions = [[0,1], [1,0], [-1,0], [0,-1]]
+            directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
             image[i][j] = newColor
 
             for dir in directions:
@@ -22,6 +23,6 @@ class Solution:
 
                 if r >= 0 and r < len(image) and c >= 0 and c < len(image[0]) and image[r][c] == oldColor:
                     recurr(image, r, c)
-        
+
         recurr(image, sr, sc)
         return image
