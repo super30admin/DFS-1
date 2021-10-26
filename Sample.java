dad7139e729c9bf2b4c382af2044ffca8c9824fd
panth1921 <panthdev96@gmail.com>
2021-10-26 00:18:52 -0400

    DFS-1

--- Sample.java
+++ Sample.java
@@ -1,7 +1,46 @@
-// Time Complexity :
-// Space Complexity :
+// Time Complexity : 0(N)
+// Space Complexity : 0(N)
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
 
 
 // Your code here along with comments explaining your approach
+
+    func floodFill(_  image: [[Int]], _ sr: Int, _ sc: Int, _ newColor: Int) -> [[Int]] {
+        var image = image
+        var stack = [[Int]]()
+        stack.append([sr,sc])
+        var oldColor = image[sr][sc]
+        while !stack.isEmpty {
+            print(stack)
+            var pixel = stack.removeLast()
+            var row = pixel[0]
+            var col = pixel[1]
+            image[row][col] = -1
+            if row - 1 >= 0 && image[row-1][col] == oldColor {
+                image[row-1][col] = -1
+                stack.append([row-1,col])
+            }
+            if row + 1 < image.count && image[row+1][col] == oldColor {
+                image[row+1][col] = -1
+                stack.append([row+1,col])
+            }
+            if col - 1 >= 0 && image[row][col-1] == oldColor {
+                image[row][col-1] = -1
+                stack.append([row,col-1])
+            } 
+            if col + 1 < image[0].count && image[row][col+1] == oldColor {
+                image[row][col+1] = -1
+                stack.append([row,col+1])
+            }
+            
+        }
+        for row in 0..<image.count {
+            for col in 0..<image[0].count {
+                if image[row][col] == -1 {
+                    image[row][col] = newColor
+                }
+            }
+        }
+        return image
+    }
---
+++ updatemat.swift
@@ -0,0 +1,60 @@
+// Time Complexity : 0(N)
+// Space Complexity : 0(N)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+    func updateMatrix(_ mat: [[Int]]) -> [[Int]] {
+        var mat = mat
+        if mat == nil || mat.count == 0 || mat[0].count == 0 {
+            return mat
+        }
+       var queue = [[Int]]()
+        var rows = mat.count
+        var cols = mat[0].count
+       var level = 0
+       for row in 0..<rows {
+           for col in 0..<cols {
+               if mat[row][col] == 0 {
+                   queue.append([row,col])
+               }
+           }
+       }
+        // BFS
+        while !queue.isEmpty {
+            var size = queue.count
+            level += 1
+            for i in 0..<size {
+                var cell = queue.removeFirst()
+                var row = cell[0]
+                var col = cell[1]
+                if row - 1 >= 0 && mat[row-1][col] == 1  {
+                    queue.append([row-1,col])
+                    mat[row-1][col] = level + 1
+                }
+                if row + 1 < rows && mat[row+1][col] == 1  {
+                    queue.append([row+1,col])
+                    mat[row+1][col] = level + 1
+                }
+                if col - 1 >= 0 && mat[row][col-1] == 1  {
+                    queue.append([row,col-1])
+                    mat[row][col-1] = level + 1
+                }
+                if col + 1 < cols && mat[row][col+1] == 1  {
+                    queue.append([row,col+1])
+                    mat[row][col+1] = level + 1
+
+                }
+            }
+        }
+        for i in 0..<rows {
+            for j in 0..<cols {
+                if mat[i][j] > 1 {
+                    mat[i][j] = mat[i][j] - 1
+                }
+            }
+        }
+        return mat
+    }
