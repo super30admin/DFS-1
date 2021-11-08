35bf217244b1d77030138d1dee6cd186075c0244
Adhish Shrivastava <adhish70@gmail.com>
2021-11-08 13:02:02 -0500

    Initial Commit

---
+++ Problem-64 Flood Fill.py
@@ -0,0 +1,11 @@
+# 733. Flood Fill
+# https://leetcode.com/problems/flood-fill/
+
+# Logic: 
+
+# Time Complexiety: 
+# Space Complexiety: 
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        pass
---
+++ Problem-65 01 Matrix.py
@@ -0,0 +1,11 @@
+# 542. 01 Matrix
+# https://leetcode.com/problems/01-matrix/
+
+# Logic: 
+
+# Time Complexiety: 
+# Space Complexiety: 
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        pass
