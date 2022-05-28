89ff26be0403ab64d017f942995d3a9e0df32cec
Rincy Soman <rincysoman@gmail.com>
2022-05-27 20:09:13 -0700

    0-1 Matrix

--- FloodFill.cs
+++ FloodFill.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 
 namespace Algorithms
 {
-    /// Time Complexity :  O(N)
-    // Space Complexity :O(N) 
+    /// Time Complexity :  O(MN)
+    // Space Complexity :O(MN) 
     // Did this code successfully run on Leetcode :Yes
     // Any problem you faced while coding this :  No
     public class FloodFill
