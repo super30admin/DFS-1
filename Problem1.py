cff753d0a52e1df7a2b03420f1758e35ea16e0cc
Pravin Kadam <pravinkadam@Pravins-Air.lan>
2022-05-31 20:05:02 -0500

    Completed DFS-1

--- Problem1.py
+++ Problem1.py
@@ -1,5 +1,7 @@
+#BFS
+
 #Time Complexity : O(M*N)
-#Space Complexity : O(N)
+#Space Complexity : O(M*N)
 #Did this code successfully run on Leetcode : YES
 #Any problem you faced while coding this : NO
 
@@ -23,4 +25,30 @@ class Solution:
                     queue.append([r,c])
                     
         return image
+
+
+#DFS
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        if image == None or image[sr][sc] == newColor:
+            return image
+        dirr = [[1,0],[0,1],[-1,0],[0,-1]]
+        row = len(image)
+        col = len(image[0])
+        color = image[sr][sc]
+        self.dfs(image,sr,sc,newColor,color,dirr,row,col)
+        return image
+        
+    def dfs(self,image,sr,sc,newColor,color,dirr,row,col):
+        #base
+        if sr < 0 or sc < 0 or sr >= row or sc >= col or image[sr][sc] != color:
+            return
+        #logic
+        image[sr][sc] = newColor
+        for i in dirr:
+            r = i[0] + sr
+            c = i[1] + sc
+            self.dfs(image,r,c,newColor,color,dirr,row,col)
+                
                 
