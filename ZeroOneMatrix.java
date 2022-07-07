ca3791762ccc038027e47c6746d153c61c6def8b
Aditya Ganesh Satish <satish.a@northeastern.edu>
2022-07-07 15:21:42 -0400

    DFS1

---
+++ ZeroOneMatrix.java
@@ -0,0 +1,38 @@
+import java.util.*;
+// TIme complexity: O(m*n)
+// Space complexity: O(m*n)
+
+public class ZeroOneMatrix {
+    int[][] dirs;
+    public int[][] updateMatrix(int[][] mat) {
+        if(mat == null || mat.length == 0) return mat;
+        int m = mat.length;
+        int n = mat[0].length;
+        Queue<int[]> q =  new LinkedList<>();
+        dirs = new int[][]{{0,1}, {1,0}, {-1,0}, {0,-1}};
+        for(int i =0; i <m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j] == 0)
+                    q.add(new int[]{i,j});
+                else mat[i][j] = -1;
+            }
+        }
+        int dist = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0; i < size;i++){
+                int[] curr = q.poll();
+                for(int[] dir: dirs){
+                    int nr = curr[0] + dir[0];
+                    int nc = curr[1] + dir[1];
+                    if(nr >=0 && nc>=0 && nr <m && nc<n && mat[nr][nc] == -1){
+                        mat[nr][nc] = dist+1;
+                        q.add(new int[]{nr, nc});
+                    }
+                }
+            }
+            dist++;
+        }
+        return mat;
+    }
+}
