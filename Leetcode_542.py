ffaa247ae591163dc080f6b11bed7ac12232afe7
Rutvik Avaiya <rutvikavaiya01@gmail.com>
2022-07-26 12:25:18 -0500

    Spring Completed

---
+++ Leetcode_542.py
@@ -0,0 +1,34 @@
+#Time Complexity: O(mn)
+#Space Complexity: O(mn)
+
+from typing import Deque
+
+
+class Solution:
+  def updateMatrix(self, mat):
+    m = len(mat)
+    n = len(mat[0])
+    dirs = [0, 1, 0, -1, 0]
+    q = Deque()
+    seen = [[False] * n for _ in range(m)]
+
+    for i in range(m):
+      for j in range(n):
+        if mat[i][j] == 0:
+          q.append((i, j))
+          seen[i][j] = True
+
+    while q:
+      i, j = q.popleft()
+      for k in range(4):
+        x = i + dirs[k]
+        y = j + dirs[k + 1]
+        if x < 0 or x == m or y < 0 or y == n:
+          continue
+        if seen[x][y]:
+          continue
+        mat[x][y] = mat[i][j] + 1
+        q.append((x, y))
+        seen[x][y] = True
+
+    return mat
