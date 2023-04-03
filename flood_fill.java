dc745aa3cec0c6f6477a24d738e4870affdc5dbb
virat02 <virat.goradia96@gmail.com>
2023-04-02 19:29:21 -0700

    Implement flood fill

---
+++ flood_fill.java
@@ -0,0 +1,34 @@
+class Solution {
+    private static final int[][] DIRS = new int[][] { {-1,0}, {1, 0}, {0, 1}, {0, -1}};
+    
+    private void dfs(int[][] image, int sr, int sc, int currColor, int targetColor) {
+        // base cases
+        if(
+            sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || 
+            currColor == targetColor || 
+            image[sr][sc] != currColor
+        ) {
+            return;
+        }
+
+        // logic
+        image[sr][sc] = targetColor;
+
+        for(int[] dir : DIRS) {
+            dfs(image, sr + dir[0], sc + dir[1], currColor, targetColor);
+        }
+    }
+
+    // Time Complexity : O(N)
+    // Space Complexity : O(N)
+    // Did this code successfully run on Leetcode : Yes
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if(image == null || image.length == 0) {
+            return image;
+        }
+
+        dfs(image, sr, sc, image[sr][sc], color);
+
+        return image;
+    }
+}
