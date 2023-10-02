7301fdc2a328fa2ed4ae10de15421745c018ee38
anaghayn <anagha2605@gmail.com>
2023-10-02 16:35:23 -0700

    Update floodFill.py

--- floodFill.py
+++ floodFill.py
@@ -10,24 +10,41 @@ class Solution(object):
         :type color: int
         :rtype: List[List[int]]
         """
-        queue = collections.deque()
+       
         m = len(image)
         n = len(image[0])
         original_Color = image[sr][sc]
         dir = [[0,-1], [0,1], [1,0], [-1,0]]
         if image[sr][sc] == color:
             return image
-        image[sr][sc] = color
-        queue.append([sr,sc])
+            
+##################  BFS  #####################
         
-        while queue:
-            for i in range(len(queue)):
-                curr_pos = queue.popleft()
-                for d in dir:
-                    newRow = curr_pos[0]+d[0]
-                    newCol = curr_pos[1]+d[1]
+        # queue = collections.deque()
+        # image[sr][sc] = color
+        # queue.append([sr,sc])
+        
+        # while queue:
+        #     for i in range(len(queue)):
+        #         curr_pos = queue.popleft()
+        #         for d in dir:
+        #             newRow = curr_pos[0]+d[0]
+        #             newCol = curr_pos[1]+d[1]
 
-                    if 0<=newRow<m and 0<=newCol<n and image[newRow][newCol] == original_Color:
-                        image[newRow][newCol] = color
-                        queue.append([newRow,newCol])
+        #             if 0<=newRow<m and 0<=newCol<n and image[newRow][newCol] == original_Color:
+        #                 image[newRow][newCol] = color
+        #                 queue.append([newRow,newCol])
+        # return image
+        
+##################  DFS  #####################
+
+        def dfs(image, row, col, color):
+            if 0 >row or row==m or 0 >col or col==n or image[row][col] != original_Color:
+                return
+            image[row][col] = color
+            for d in dir:
+                newRow = row+d[0]
+                newCol = col+d[1]
+                dfs(image,newRow,newCol,color)
+        dfs(image, sr, sc, color)
         return image
