64f25c9f40b1525cb2c3cb02485ae9805348c777
Yatish Koul <36380820+yatishkoul@users.noreply.github.com>
2023-10-27 21:54:56 -0700

    Update Flood_Fill.cpp

--- Flood_Fill.cpp
+++ Flood_Fill.cpp
@@ -30,3 +30,35 @@ public:
         return image;
     }
 };
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+class Solution {
+private:
+    int dir[4][2]={{0,1}, {0,-1}, {1,0}, {-1,0}};
+    int oldColor;
+
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if(image.size()==0 )return image;
+        oldColor = image[sr][sc];
+        if(image[sr][sc]==color)return image;
+        int m=image.size(), n=image[0].size();
+        dfs(image, sr, sc, color, m, n);
+        return image;
+    }
+private:
+    void dfs(vector<vector<int>>& image, int sr, int sc, int color, int m, int n){
+        //base case
+        if(sr<0 || sr==m || sc==n || sc<0 || image[sr][sc]!=oldColor){
+            return;
+        }
+
+        //logic
+        image[sr][sc]=color;
+        for(int i=0;i<4;i++){
+            int nr = dir[i][0] + sr; 
+            int nc = dir[i][1] + sc;
+            dfs(image, nr, nc, color, m, n);
+        }
+
+    }    
+};
